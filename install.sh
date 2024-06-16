d61aa8d2a9239aa1167a9c3f0ae35eb3ac2baf72
lion537 <144499080+lion537@users.noreply.github.com>
2024-06-16 16:48:54 -0300

    Update install.sh
    update

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #install ubuntu 20.04
+wget https://multicraft.org/files/multicraft-2.5.0-pre7-64.tar.gz
+tar xvzf multicraft-2.5.0-pre7-64.tar.gz
 apt update
 apt upgrade -y
 apt install lamp-server^
 apt install php-{gd,mysql,mbstring,sqlite3,zip} sqlite curl unzip openjdk-8-jre openjdk-21-jre
+
+cd multicraft ./install.sh
