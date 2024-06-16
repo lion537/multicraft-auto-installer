d3a4dabf4cc55c756d231b44182fc5d694585cd6
lion537 <144499080+lion537@users.noreply.github.com>
2024-06-16 16:56:10 -0300

    Update install.sh
    add apache2 conf

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #install ubuntu 20.04
 wget https://multicraft.org/files/multicraft-2.5.0-pre7-64.tar.gz
+wget https://raw.githubusercontent.com/lion537/multicraft-auto-installer/main/apache2.conf
 tar xvzf multicraft-2.5.0-pre7-64.tar.gz
 apt update
 apt upgrade -y
 apt install lamp-server^
 apt install php-{gd,mysql,mbstring,sqlite3,zip} sqlite curl unzip zip openjdk-8-jre openjdk-21-jre
-
+mv apache2.conf /etc/apache2/
 cd multicraft ./setup.sh
